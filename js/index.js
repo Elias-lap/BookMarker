886b07a557749ce239aa4d22759f9bdee468c57d
Elias-lap <eliaszerz92@gmail.com>
2023-02-08 01:07:20 +0100

    Elias zerz /c40 / wed&sun

---
+++ js/index.js
@@ -0,0 +1,57 @@
+var siteNameInput=document.getElementById(`siteName`)
+var SiteURLInput=document.getElementById(`SiteURL`)
+var MainCollection = []
+if ( localStorage.getItem('collection')!=null){
+    MainCollection=JSON.parse(localStorage.getItem('collection'))
+    displayCollection(MainCollection)
+    
+    
+
+}
+
+function DataCollection(){
+    var Collection ={
+        NameBook:siteNameInput.value,
+        SiteBook:SiteURLInput.value
+    }
+    MainCollection.push(Collection)
+    localStorage.setItem('collection' ,JSON.stringify(MainCollection) )
+    displayCollection(MainCollection)
+    clearInput()
+}
+var count = '0'
+function displayCollection(arr){
+    var trs =``
+    for ( var i = 0 ; i < arr.length; i++){
+        count=i
+         trs+=`<tr>
+        <td>${i}</td>
+        <td>${arr[i].NameBook}</td>
+        <td>${arr[i].SiteBook}</td>
+        <td><button class="btn btn-danger" onclick="Visit(${i})"id="visitBtn">Visit</button></td>
+        <td><button class="btn btn-info" onclick="deletInput(${i})">Delet</button></td>
+    </tr>`
+    
+    }
+    document.getElementById`tbody`.innerHTML=trs
+  
+   
+    }
+function clearInput(){
+    siteNameInput.value=``
+    SiteURLInput.value=``
+}
+function deletInput(bookcount){
+    MainCollection.splice(bookcount,1);
+    localStorage.setItem('collection' ,JSON.stringify(MainCollection) );
+    displayCollection(MainCollection)
+}
+function Visit(count){
+    window.open(MainCollection[count].SiteBook, "_blank");
+
+}
+
+
+// visitBtn.addEventListener("click", function() {
+//     window.open(MainCollection[count].SiteBook, "_blank");
+//   });
